a5e213239b77500b6cba6eea7794acdaa9f6e9c0
divyansh-palcementAdda <divyanshtomar8319@gmail.com>
2026-07-13 11:20:10 +0530

    updated

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -4,13 +4,13 @@ export const environment = {
   // wsUrl: 'http://localhost:8081/ws-notifications',
   // apiUrl: 'http://localhost:8081/api',
   // apiUrl: 'http://172.23.112.1:8081/api',
-  // production: true,
-  // apiUrl: 'https://api.areyoureporting.com/api',
-  // wsUrl: 'https://api.areyoureporting.com/ws-notifications'
-
   production: true,
-  apiUrl: 'https://dev.areyoureporting.com/api',
-  wsUrl: 'https://dev.areyoureporting.com/ws-notifications',
+  apiUrl: 'https://api.areyoureporting.com/api',
+  wsUrl: 'https://api.areyoureporting.com/ws-notifications',
+
+  // production: true,
+  // apiUrl: 'https://dev.areyoureporting.com/api',
+  // wsUrl: 'https://dev.areyoureporting.com/ws-notifications',
   integrationApiKey: 'Q0NwV0lJc2tVQk1jVjR6R2hSYjV4TjNwWmV2M2hJQW5Kc1RjM0F5Z0RrR1Q='
 };
 
